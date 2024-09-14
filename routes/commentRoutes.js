2c141c770a09faf6bf66f28a4f8c090a534d8190
NikitaGhimire <nikitaghimire1@gmail.com>
2024-09-14 18:19:38 +1000

    Added a comment route to add and delete comment for authenticated users

---
+++ routes/commentRoutes.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+require("dotenv").config();
+
+//add a comment to a post(authenticated users)
+router.post(
+  "/add",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { content, postId } = req.body;
+    const userId = req.user.id; // Get user ID from the authenticated user
+
+    try {
+      const comment = await prisma.comment.create({
+        data: {
+          content,
+          postId,
+          userId,
+          username: req.user.username, // Assuming the user object has a username field
+        },
+      });
+      res.status(201).json(comment);
+    } catch (err) {
+      res.status(500).json({ error: "Error adding comment" });
+    }
+  }
+);
+
+// Delete a comment (authenticated users only)
+router.delete(
+  "/delete/:id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { id } = req.params;
+    const userId = req.user.id; // Get user ID from the authenticated user
+
+    try {
+      // Find the comment by ID
+      const comment = await prisma.comment.findUnique({
+        where: { id: parseInt(id) },
+      });
+
+      if (!comment) {
+        return res.status(404).json({ error: "Comment not found" });
+      }
+
+      // Check if the comment belongs to the user
+      if (comment.userId !== userId) {
+        return res
+          .status(403)
+          .json({ error: "Unauthorized to delete this comment" });
+      }
+
+      // Delete the comment
+      await prisma.comment.delete({ where: { id: parseInt(id) } });
+      res.json({ message: "Comment deleted successfully" });
+    } catch (err) {
+      res.status(500).json({ error: "Error deleting comment" });
+    }
+  }
+);
+
+module.exports = router;
