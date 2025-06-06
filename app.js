804a6fbe00ff07dfb5cf576f5038acad86806e10
NikitaGhimire <nikitaghimire1@gmail.com>
2025-06-06 21:10:08 +1000

    cors issue fix

--- app.js
+++ app.js
@@ -12,9 +12,20 @@ const commentRoutes = require("./routes/commentRoutes");
 //initialise app
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://my-blogpost-frontend.vercel.app"
+];
+
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
